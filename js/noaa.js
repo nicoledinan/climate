673ad8b2e80cf8af36774bb89b878ca61e62b797
nicoledinan <ncdinan@gmail.com>
2020-01-26 14:11:50 -0500

    Initial commit

---
+++ js/noaa.js
@@ -0,0 +1,1107 @@
+var noaa =[
+  {
+    "Year": 2016,
+    "celsius": 0.95,
+    "farenheit": 1.71,
+    "rank": 1,
+    "averagef": 58.71,
+    "averagec": 14.85
+  },
+  {
+    "Year": 2015,
+    "celsius": 0.91,
+    "farenheit": 1.638,
+    "rank": 2,
+    "averagef": 58.638,
+    "averagec": 14.81
+  },
+  {
+    "Year": 2014,
+    "celsius": 0.75,
+    "farenheit": 1.35,
+    "rank": 3,
+    "averagef": 58.35,
+    "averagec": 14.65
+  },
+  {
+    "Year": 2010,
+    "celsius": 0.7,
+    "farenheit": 1.26,
+    "rank": 4,
+    "averagef": 58.26,
+    "averagec": 14.6
+  },
+  {
+    "Year": 2013,
+    "celsius": 0.67,
+    "farenheit": 1.206,
+    "rank": 5,
+    "averagef": 58.206,
+    "averagec": 14.57
+  },
+  {
+    "Year": 2005,
+    "celsius": 0.66,
+    "farenheit": 1.188,
+    "rank": 6,
+    "averagef": 58.188,
+    "averagec": 14.56
+  },
+  {
+    "Year": 1998,
+    "celsius": 0.64,
+    "farenheit": 1.152,
+    "rank": 7,
+    "averagef": 58.152,
+    "averagec": 14.54
+  },
+  {
+    "Year": 2009,
+    "celsius": 0.64,
+    "farenheit": 1.152,
+    "rank": 8,
+    "averagef": 58.152,
+    "averagec": 14.54
+  },
+  {
+    "Year": 2006,
+    "celsius": 0.62,
+    "farenheit": 1.116,
+    "rank": 9,
+    "averagef": 58.116,
+    "averagec": 14.52
+  },
+  {
+    "Year": 2012,
+    "celsius": 0.62,
+    "farenheit": 1.116,
+    "rank": 10,
+    "averagef": 58.116,
+    "averagec": 14.52
+  },
+  {
+    "Year": 2003,
+    "celsius": 0.61,
+    "farenheit": 1.098,
+    "rank": 11,
+    "averagef": 58.098,
+    "averagec": 14.51
+  },
+  {
+    "Year": 2007,
+    "celsius": 0.61,
+    "farenheit": 1.098,
+    "rank": 12,
+    "averagef": 58.098,
+    "averagec": 14.51
+  },
+  {
+    "Year": 2002,
+    "celsius": 0.6,
+    "farenheit": 1.08,
+    "rank": 13,
+    "averagef": 58.08,
+    "averagec": 14.5
+  },
+  {
+    "Year": 2004,
+    "celsius": 0.58,
+    "farenheit": 1.044,
+    "rank": 14,
+    "averagef": 58.044,
+    "averagec": 14.48
+  },
+  {
+    "Year": 2011,
+    "celsius": 0.58,
+    "farenheit": 1.044,
+    "rank": 15,
+    "averagef": 58.044,
+    "averagec": 14.48
+  },
+  {
+    "Year": 2001,
+    "celsius": 0.55,
+    "farenheit": 0.99,
+    "rank": 16,
+    "averagef": 57.99,
+    "averagec": 14.45
+  },
+  {
+    "Year": 2008,
+    "celsius": 0.54,
+    "farenheit": 0.972,
+    "rank": 17,
+    "averagef": 57.972,
+    "averagec": 14.44
+  },
+  {
+    "Year": 1997,
+    "celsius": 0.52,
+    "farenheit": 0.936,
+    "rank": 18,
+    "averagef": 57.936,
+    "averagec": 14.42
+  },
+  {
+    "Year": 1995,
+    "celsius": 0.46,
+    "farenheit": 0.828,
+    "rank": 19,
+    "averagef": 57.828,
+    "averagec": 14.36
+  },
+  {
+    "Year": 1999,
+    "celsius": 0.45,
+    "farenheit": 0.81,
+    "rank": 20,
+    "averagef": 57.81,
+    "averagec": 14.35
+  },
+  {
+    "Year": 1990,
+    "celsius": 0.43,
+    "farenheit": 0.774,
+    "rank": 21,
+    "averagef": 57.774,
+    "averagec": 14.33
+  },
+  {
+    "Year": 2000,
+    "celsius": 0.43,
+    "farenheit": 0.774,
+    "rank": 22,
+    "averagef": 57.774,
+    "averagec": 14.33
+  },
+  {
+    "Year": 1991,
+    "celsius": 0.4,
+    "farenheit": 0.72,
+    "rank": 23,
+    "averagef": 57.72,
+    "averagec": 14.3
+  },
+  {
+    "Year": 1988,
+    "celsius": 0.38,
+    "farenheit": 0.684,
+    "rank": 24,
+    "averagef": 57.684,
+    "averagec": 14.28
+  },
+  {
+    "Year": 1987,
+    "celsius": 0.37,
+    "farenheit": 0.666,
+    "rank": 25,
+    "averagef": 57.666,
+    "averagec": 14.27
+  },
+  {
+    "Year": 1983,
+    "celsius": 0.34,
+    "farenheit": 0.612,
+    "rank": 26,
+    "averagef": 57.612,
+    "averagec": 14.24
+  },
+  {
+    "Year": 1994,
+    "celsius": 0.34,
+    "farenheit": 0.612,
+    "rank": 27,
+    "averagef": 57.612,
+    "averagec": 14.24
+  },
+  {
+    "Year": 1996,
+    "celsius": 0.33,
+    "farenheit": 0.594,
+    "rank": 28,
+    "averagef": 57.594,
+    "averagec": 14.23
+  },
+  {
+    "Year": 1981,
+    "celsius": 0.3,
+    "farenheit": 0.54,
+    "rank": 29,
+    "averagef": 57.54,
+    "averagec": 14.2
+  },
+  {
+    "Year": 1989,
+    "celsius": 0.3,
+    "farenheit": 0.54,
+    "rank": 30,
+    "averagef": 57.54,
+    "averagec": 14.2
+  },
+  {
+    "Year": 1944,
+    "celsius": 0.29,
+    "farenheit": 0.522,
+    "rank": 31,
+    "averagef": 57.522,
+    "averagec": 14.19
+  },
+  {
+    "Year": 1993,
+    "celsius": 0.29,
+    "farenheit": 0.522,
+    "rank": 32,
+    "averagef": 57.522,
+    "averagec": 14.19
+  },
+  {
+    "Year": 1980,
+    "celsius": 0.27,
+    "farenheit": 0.486,
+    "rank": 33,
+    "averagef": 57.486,
+    "averagec": 14.17
+  },
+  {
+    "Year": 1992,
+    "celsius": 0.26,
+    "farenheit": 0.468,
+    "rank": 34,
+    "averagef": 57.468,
+    "averagec": 14.16
+  },
+  {
+    "Year": 1979,
+    "celsius": 0.23,
+    "farenheit": 0.414,
+    "rank": 35,
+    "averagef": 57.414,
+    "averagec": 14.13
+  },
+  {
+    "Year": 1986,
+    "celsius": 0.23,
+    "farenheit": 0.414,
+    "rank": 36,
+    "averagef": 57.414,
+    "averagec": 14.13
+  },
+  {
+    "Year": 1977,
+    "celsius": 0.2,
+    "farenheit": 0.36,
+    "rank": 37,
+    "averagef": 57.36,
+    "averagec": 14.1
+  },
+  {
+    "Year": 1941,
+    "celsius": 0.19,
+    "farenheit": 0.342,
+    "rank": 38,
+    "averagef": 57.342,
+    "averagec": 14.09
+  },
+  {
+    "Year": 1982,
+    "celsius": 0.18,
+    "farenheit": 0.324,
+    "rank": 39,
+    "averagef": 57.324,
+    "averagec": 14.08
+  },
+  {
+    "Year": 1945,
+    "celsius": 0.16,
+    "farenheit": 0.288,
+    "rank": 40,
+    "averagef": 57.288,
+    "averagec": 14.06
+  },
+  {
+    "Year": 1973,
+    "celsius": 0.16,
+    "farenheit": 0.288,
+    "rank": 41,
+    "averagef": 57.288,
+    "averagec": 14.06
+  },
+  {
+    "Year": 1942,
+    "celsius": 0.15,
+    "farenheit": 0.27,
+    "rank": 42,
+    "averagef": 57.27,
+    "averagec": 14.05
+  },
+  {
+    "Year": 1943,
+    "celsius": 0.15,
+    "farenheit": 0.27,
+    "rank": 43,
+    "averagef": 57.27,
+    "averagec": 14.05
+  },
+  {
+    "Year": 1984,
+    "celsius": 0.15,
+    "farenheit": 0.27,
+    "rank": 44,
+    "averagef": 57.27,
+    "averagec": 14.05
+  },
+  {
+    "Year": 1985,
+    "celsius": 0.14,
+    "farenheit": 0.252,
+    "rank": 45,
+    "averagef": 57.252,
+    "averagec": 14.04
+  },
+  {
+    "Year": 1958,
+    "celsius": 0.11,
+    "farenheit": 0.198,
+    "rank": 46,
+    "averagef": 57.198,
+    "averagec": 14.01
+  },
+  {
+    "Year": 1978,
+    "celsius": 0.11,
+    "farenheit": 0.198,
+    "rank": 47,
+    "averagef": 57.198,
+    "averagec": 14.01
+  },
+  {
+    "Year": 1963,
+    "celsius": 0.1,
+    "farenheit": 0.18,
+    "rank": 48,
+    "averagef": 57.18,
+    "averagec": 14
+  },
+  {
+    "Year": 1940,
+    "celsius": 0.09,
+    "farenheit": 0.162,
+    "rank": 49,
+    "averagef": 57.162,
+    "averagec": 13.99
+  },
+  {
+    "Year": 1953,
+    "celsius": 0.09,
+    "farenheit": 0.162,
+    "rank": 50,
+    "averagef": 57.162,
+    "averagec": 13.99
+  },
+  {
+    "Year": 1962,
+    "celsius": 0.09,
+    "farenheit": 0.162,
+    "rank": 51,
+    "averagef": 57.162,
+    "averagec": 13.99
+  },
+  {
+    "Year": 1969,
+    "celsius": 0.09,
+    "farenheit": 0.162,
+    "rank": 52,
+    "averagef": 57.162,
+    "averagec": 13.99
+  },
+  {
+    "Year": 1961,
+    "celsius": 0.07,
+    "farenheit": 0.126,
+    "rank": 53,
+    "averagef": 57.126,
+    "averagec": 13.97
+  },
+  {
+    "Year": 1959,
+    "celsius": 0.05,
+    "farenheit": 0.09,
+    "rank": 54,
+    "averagef": 57.09,
+    "averagec": 13.95
+  },
+  {
+    "Year": 1957,
+    "celsius": 0.04,
+    "farenheit": 0.072,
+    "rank": 55,
+    "averagef": 57.072,
+    "averagec": 13.94
+  },
+  {
+    "Year": 1970,
+    "celsius": 0.04,
+    "farenheit": 0.072,
+    "rank": 56,
+    "averagef": 57.072,
+    "averagec": 13.94
+  },
+  {
+    "Year": 1972,
+    "celsius": 0.03,
+    "farenheit": 0.054,
+    "rank": 57,
+    "averagef": 57.054,
+    "averagec": 13.93
+  },
+  {
+    "Year": 1952,
+    "celsius": 0.02,
+    "farenheit": 0.036,
+    "rank": 58,
+    "averagef": 57.036,
+    "averagec": 13.92
+  },
+  {
+    "Year": 1960,
+    "celsius": 0.02,
+    "farenheit": 0.036,
+    "rank": 59,
+    "averagef": 57.036,
+    "averagec": 13.92
+  },
+  {
+    "Year": 1975,
+    "celsius": 0,
+    "farenheit": 0,
+    "rank": 60,
+    "averagef": 57,
+    "averagec": 13.9
+  },
+  {
+    "Year": 1946,
+    "celsius": -0.01,
+    "farenheit": -0.018,
+    "rank": 61,
+    "averagef": 56.982,
+    "averagec": 13.89
+  },
+  {
+    "Year": 1937,
+    "celsius": -0.02,
+    "farenheit": -0.036,
+    "rank": 62,
+    "averagef": 56.964,
+    "averagec": 13.88
+  },
+  {
+    "Year": 1939,
+    "celsius": -0.02,
+    "farenheit": -0.036,
+    "rank": 63,
+    "averagef": 56.964,
+    "averagec": 13.88
+  },
+  {
+    "Year": 1951,
+    "celsius": -0.02,
+    "farenheit": -0.036,
+    "rank": 64,
+    "averagef": 56.964,
+    "averagec": 13.88
+  },
+  {
+    "Year": 1966,
+    "celsius": -0.02,
+    "farenheit": -0.036,
+    "rank": 65,
+    "averagef": 56.964,
+    "averagec": 13.88
+  },
+  {
+    "Year": 1967,
+    "celsius": -0.02,
+    "farenheit": -0.036,
+    "rank": 66,
+    "averagef": 56.964,
+    "averagec": 13.88
+  },
+  {
+    "Year": 1938,
+    "celsius": -0.03,
+    "farenheit": -0.054,
+    "rank": 67,
+    "averagef": 56.946,
+    "averagec": 13.87
+  },
+  {
+    "Year": 1968,
+    "celsius": -0.03,
+    "farenheit": -0.054,
+    "rank": 68,
+    "averagef": 56.946,
+    "averagec": 13.87
+  },
+  {
+    "Year": 1926,
+    "celsius": -0.05,
+    "farenheit": -0.09,
+    "rank": 69,
+    "averagef": 56.91,
+    "averagec": 13.85
+  },
+  {
+    "Year": 1947,
+    "celsius": -0.06,
+    "farenheit": -0.108,
+    "rank": 70,
+    "averagef": 56.892,
+    "averagec": 13.84
+  },
+  {
+    "Year": 1948,
+    "celsius": -0.06,
+    "farenheit": -0.108,
+    "rank": 71,
+    "averagef": 56.892,
+    "averagec": 13.84
+  },
+  {
+    "Year": 1949,
+    "celsius": -0.06,
+    "farenheit": -0.108,
+    "rank": 72,
+    "averagef": 56.892,
+    "averagec": 13.84
+  },
+  {
+    "Year": 1881,
+    "celsius": -0.07,
+    "farenheit": -0.126,
+    "rank": 73,
+    "averagef": 56.874,
+    "averagec": 13.83
+  },
+  {
+    "Year": 1900,
+    "celsius": -0.07,
+    "farenheit": -0.126,
+    "rank": 74,
+    "averagef": 56.874,
+    "averagec": 13.83
+  },
+  {
+    "Year": 1915,
+    "celsius": -0.07,
+    "farenheit": -0.126,
+    "rank": 75,
+    "averagef": 56.874,
+    "averagec": 13.83
+  },
+  {
+    "Year": 1931,
+    "celsius": -0.07,
+    "farenheit": -0.126,
+    "rank": 76,
+    "averagef": 56.874,
+    "averagec": 13.83
+  },
+  {
+    "Year": 1974,
+    "celsius": -0.07,
+    "farenheit": -0.126,
+    "rank": 77,
+    "averagef": 56.874,
+    "averagec": 13.83
+  },
+  {
+    "Year": 1882,
+    "celsius": -0.08,
+    "farenheit": -0.144,
+    "rank": 78,
+    "averagef": 56.856,
+    "averagec": 13.82
+  },
+  {
+    "Year": 1965,
+    "celsius": -0.08,
+    "farenheit": -0.144,
+    "rank": 79,
+    "averagef": 56.856,
+    "averagec": 13.82
+  },
+  {
+    "Year": 1971,
+    "celsius": -0.08,
+    "farenheit": -0.144,
+    "rank": 80,
+    "averagef": 56.856,
+    "averagec": 13.82
+  },
+  {
+    "Year": 1976,
+    "celsius": -0.08,
+    "farenheit": -0.144,
+    "rank": 81,
+    "averagef": 56.856,
+    "averagec": 13.82
+  },
+  {
+    "Year": 1896,
+    "celsius": -0.09,
+    "farenheit": -0.162,
+    "rank": 82,
+    "averagef": 56.838,
+    "averagec": 13.81
+  },
+  {
+    "Year": 1930,
+    "celsius": -0.09,
+    "farenheit": -0.162,
+    "rank": 83,
+    "averagef": 56.838,
+    "averagec": 13.81
+  },
+  {
+    "Year": 1889,
+    "celsius": -0.1,
+    "farenheit": -0.18,
+    "rank": 84,
+    "averagef": 56.82,
+    "averagec": 13.8
+  },
+  {
+    "Year": 1934,
+    "celsius": -0.1,
+    "farenheit": -0.18,
+    "rank": 85,
+    "averagef": 56.82,
+    "averagec": 13.8
+  },
+  {
+    "Year": 1897,
+    "celsius": -0.11,
+    "farenheit": -0.198,
+    "rank": 86,
+    "averagef": 56.802,
+    "averagec": 13.79
+  },
+  {
+    "Year": 1936,
+    "celsius": -0.11,
+    "farenheit": -0.198,
+    "rank": 87,
+    "averagef": 56.802,
+    "averagec": 13.79
+  },
+  {
+    "Year": 1880,
+    "celsius": -0.12,
+    "farenheit": -0.216,
+    "rank": 88,
+    "averagef": 56.784,
+    "averagec": 13.78
+  },
+  {
+    "Year": 1899,
+    "celsius": -0.12,
+    "farenheit": -0.216,
+    "rank": 89,
+    "averagef": 56.784,
+    "averagec": 13.78
+  },
+  {
+    "Year": 1932,
+    "celsius": -0.12,
+    "farenheit": -0.216,
+    "rank": 90,
+    "averagef": 56.784,
+    "averagec": 13.78
+  },
+  {
+    "Year": 1954,
+    "celsius": -0.12,
+    "farenheit": -0.216,
+    "rank": 91,
+    "averagef": 56.784,
+    "averagec": 13.78
+  },
+  {
+    "Year": 1901,
+    "celsius": -0.14,
+    "farenheit": -0.252,
+    "rank": 92,
+    "averagef": 56.748,
+    "averagec": 13.76
+  },
+  {
+    "Year": 1914,
+    "celsius": -0.14,
+    "farenheit": -0.252,
+    "rank": 93,
+    "averagef": 56.748,
+    "averagec": 13.76
+  },
+  {
+    "Year": 1921,
+    "celsius": -0.14,
+    "farenheit": -0.252,
+    "rank": 94,
+    "averagef": 56.748,
+    "averagec": 13.76
+  },
+  {
+    "Year": 1925,
+    "celsius": -0.14,
+    "farenheit": -0.252,
+    "rank": 95,
+    "averagef": 56.748,
+    "averagec": 13.76
+  },
+  {
+    "Year": 1927,
+    "celsius": -0.14,
+    "farenheit": -0.252,
+    "rank": 96,
+    "averagef": 56.748,
+    "averagec": 13.76
+  },
+  {
+    "Year": 1935,
+    "celsius": -0.14,
+    "farenheit": -0.252,
+    "rank": 97,
+    "averagef": 56.748,
+    "averagec": 13.76
+  },
+  {
+    "Year": 1955,
+    "celsius": -0.14,
+    "farenheit": -0.252,
+    "rank": 98,
+    "averagef": 56.748,
+    "averagec": 13.76
+  },
+  {
+    "Year": 1883,
+    "celsius": -0.15,
+    "farenheit": -0.27,
+    "rank": 99,
+    "averagef": 56.73,
+    "averagec": 13.75
+  },
+  {
+    "Year": 1888,
+    "celsius": -0.15,
+    "farenheit": -0.27,
+    "rank": 100,
+    "averagef": 56.73,
+    "averagec": 13.75
+  },
+  {
+    "Year": 1964,
+    "celsius": -0.15,
+    "farenheit": -0.27,
+    "rank": 101,
+    "averagef": 56.73,
+    "averagec": 13.75
+  },
+  {
+    "Year": 1928,
+    "celsius": -0.16,
+    "farenheit": -0.288,
+    "rank": 102,
+    "averagef": 56.712,
+    "averagec": 13.74
+  },
+  {
+    "Year": 1950,
+    "celsius": -0.17,
+    "farenheit": -0.306,
+    "rank": 103,
+    "averagef": 56.694,
+    "averagec": 13.73
+  },
+  {
+    "Year": 1918,
+    "celsius": -0.2,
+    "farenheit": -0.36,
+    "rank": 104,
+    "averagef": 56.64,
+    "averagec": 13.7
+  },
+  {
+    "Year": 1919,
+    "celsius": -0.2,
+    "farenheit": -0.36,
+    "rank": 105,
+    "averagef": 56.64,
+    "averagec": 13.7
+  },
+  {
+    "Year": 1886,
+    "celsius": -0.21,
+    "farenheit": -0.378,
+    "rank": 106,
+    "averagef": 56.622,
+    "averagec": 13.69
+  },
+  {
+    "Year": 1906,
+    "celsius": -0.21,
+    "farenheit": -0.378,
+    "rank": 107,
+    "averagef": 56.622,
+    "averagec": 13.69
+  },
+  {
+    "Year": 1920,
+    "celsius": -0.21,
+    "farenheit": -0.378,
+    "rank": 108,
+    "averagef": 56.622,
+    "averagec": 13.69
+  },
+  {
+    "Year": 1923,
+    "celsius": -0.21,
+    "farenheit": -0.378,
+    "rank": 109,
+    "averagef": 56.622,
+    "averagec": 13.69
+  },
+  {
+    "Year": 1956,
+    "celsius": -0.21,
+    "farenheit": -0.378,
+    "rank": 110,
+    "averagef": 56.622,
+    "averagec": 13.69
+  },
+  {
+    "Year": 1884,
+    "celsius": -0.22,
+    "farenheit": -0.396,
+    "rank": 111,
+    "averagef": 56.604,
+    "averagec": 13.68
+  },
+  {
+    "Year": 1885,
+    "celsius": -0.22,
+    "farenheit": -0.396,
+    "rank": 112,
+    "averagef": 56.604,
+    "averagec": 13.68
+  },
+  {
+    "Year": 1895,
+    "celsius": -0.22,
+    "farenheit": -0.396,
+    "rank": 113,
+    "averagef": 56.604,
+    "averagec": 13.68
+  },
+  {
+    "Year": 1922,
+    "celsius": -0.22,
+    "farenheit": -0.396,
+    "rank": 114,
+    "averagef": 56.604,
+    "averagec": 13.68
+  },
+  {
+    "Year": 1924,
+    "celsius": -0.24,
+    "farenheit": -0.432,
+    "rank": 115,
+    "averagef": 56.568,
+    "averagec": 13.66
+  },
+  {
+    "Year": 1887,
+    "celsius": -0.25,
+    "farenheit": -0.45,
+    "rank": 116,
+    "averagef": 56.55,
+    "averagec": 13.65
+  },
+  {
+    "Year": 1891,
+    "celsius": -0.25,
+    "farenheit": -0.45,
+    "rank": 117,
+    "averagef": 56.55,
+    "averagec": 13.65
+  },
+  {
+    "Year": 1902,
+    "celsius": -0.25,
+    "farenheit": -0.45,
+    "rank": 118,
+    "averagef": 56.55,
+    "averagec": 13.65
+  },
+  {
+    "Year": 1933,
+    "celsius": -0.25,
+    "farenheit": -0.45,
+    "rank": 119,
+    "averagef": 56.55,
+    "averagec": 13.65
+  },
+  {
+    "Year": 1898,
+    "celsius": -0.26,
+    "farenheit": -0.468,
+    "rank": 120,
+    "averagef": 56.532,
+    "averagec": 13.64
+  },
+  {
+    "Year": 1894,
+    "celsius": -0.28,
+    "farenheit": -0.504,
+    "rank": 121,
+    "averagef": 56.496,
+    "averagec": 13.62
+  },
+  {
+    "Year": 1905,
+    "celsius": -0.29,
+    "farenheit": -0.522,
+    "rank": 122,
+    "averagef": 56.478,
+    "averagec": 13.61
+  },
+  {
+    "Year": 1916,
+    "celsius": -0.29,
+    "farenheit": -0.522,
+    "rank": 123,
+    "averagef": 56.478,
+    "averagec": 13.61
+  },
+  {
+    "Year": 1929,
+    "celsius": -0.29,
+    "farenheit": -0.522,
+    "rank": 124,
+    "averagef": 56.478,
+    "averagec": 13.61
+  },
+  {
+    "Year": 1892,
+    "celsius": -0.3,
+    "farenheit": -0.54,
+    "rank": 125,
+    "averagef": 56.46,
+    "averagec": 13.6
+  },
+  {
+    "Year": 1893,
+    "celsius": -0.31,
+    "farenheit": -0.558,
+    "rank": 126,
+    "averagef": 56.442,
+    "averagec": 13.59
+  },
+  {
+    "Year": 1917,
+    "celsius": -0.31,
+    "farenheit": -0.558,
+    "rank": 127,
+    "averagef": 56.442,
+    "averagec": 13.59
+  },
+  {
+    "Year": 1890,
+    "celsius": -0.32,
+    "farenheit": -0.576,
+    "rank": 128,
+    "averagef": 56.424,
+    "averagec": 13.58
+  },
+  {
+    "Year": 1913,
+    "celsius": -0.32,
+    "farenheit": -0.576,
+    "rank": 129,
+    "averagef": 56.424,
+    "averagec": 13.58
+  },
+  {
+    "Year": 1912,
+    "celsius": -0.33,
+    "farenheit": -0.594,
+    "rank": 130,
+    "averagef": 56.406,
+    "averagec": 13.57
+  },
+  {
+    "Year": 1903,
+    "celsius": -0.34,
+    "farenheit": -0.612,
+    "rank": 131,
+    "averagef": 56.388,
+    "averagec": 13.56
+  },
+  {
+    "Year": 1907,
+    "celsius": -0.37,
+    "farenheit": -0.666,
+    "rank": 132,
+    "averagef": 56.334,
+    "averagec": 13.53
+  },
+  {
+    "Year": 1910,
+    "celsius": -0.38,
+    "farenheit": -0.684,
+    "rank": 133,
+    "averagef": 56.316,
+    "averagec": 13.52
+  },
+  {
+    "Year": 1904,
+    "celsius": -0.42,
+    "farenheit": -0.756,
+    "rank": 134,
+    "averagef": 56.244,
+    "averagec": 13.48
+  },
+  {
+    "Year": 1909,
+    "celsius": -0.43,
+    "farenheit": -0.774,
+    "rank": 135,
+    "averagef": 56.226,
+    "averagec": 13.47
+  },
+  {
+    "Year": 1908,
+    "celsius": -0.44,
+    "farenheit": -0.792,
+    "rank": 136,
+    "averagef": 56.208,
+    "averagec": 13.46
+  },
+  {
+    "Year": 1911,
+    "celsius": -0.44,
+    "farenheit": -0.792,
+    "rank": 137,
+    "averagef": 56.208,
+    "averagec": 13.46
+  },
+  {
+    "Year": 2017,
+  },
+  {
+    "Year": 2018,
+  },
+  {
+    "Year": 2019,
+  }
+];
